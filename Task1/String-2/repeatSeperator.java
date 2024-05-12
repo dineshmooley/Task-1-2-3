678161d091c803853cfc15eae74380cf8a01ac15
V Dinesh Kumar <95033085+dineshmooley@users.noreply.github.com>
2024-05-12 09:43:47 +0530

    Create repeatSeperator.java

---
+++ Task1/String-2/repeatSeperator.java
@@ -0,0 +1,9 @@
+public String repeatSeparator(String word, String sep, int count) {
+  String ans = "";
+  if(count == 0)  return ans;
+  for(int i=0; i<count-1; i++)  {
+    ans = ans + word + sep;
+  }
+  
+  return ans + word;
+}
