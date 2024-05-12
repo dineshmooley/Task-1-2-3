4f47c8eecea8ae106d6739aebb587d6487aa5ee9
V Dinesh Kumar <95033085+dineshmooley@users.noreply.github.com>
2024-05-12 18:50:25 +0530

    Create no14.java

---
+++ Task1/Array-2/no14.java
@@ -0,0 +1,10 @@
+public boolean no14(int[] nums) {
+  int count1 = 0, count4 = 0;
+    
+  for(int i: nums)  {
+    if(i == 1)  count1++;
+    if(i == 4)  count4++;
+  }
+  
+  return count1 == 0 || count4 == 0;
+}
