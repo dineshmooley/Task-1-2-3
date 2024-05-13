7e347499e817351dcba0c7b27473c2aa8cc37725
V Dinesh Kumar <95033085+dineshmooley@users.noreply.github.com>
2024-05-13 09:05:29 +0530

    Create twotwo.java

---
+++ Task1/Array-2/twotwo.java
@@ -0,0 +1,10 @@
+public boolean twoTwo(int[] nums) {
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] == 2) {
+      if (i > 0 && nums[i - 1] == 2) continue;
+      if (i < nums.length - 1 && nums[i + 1] == 2) i++; 
+      else return false; 
+    }
+  }
+  return true;
+}
