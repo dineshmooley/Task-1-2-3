01ed5a4ee7a62cfa4ac184f651dad06e08f5e9c0
V Dinesh Kumar <95033085+dineshmooley@users.noreply.github.com>
2024-05-13 20:56:46 +0530

    Create endX.java

---
+++ Task1/Recursion-1/endX.java
@@ -0,0 +1,6 @@
+public String endX(String str) {
+  if(str.isEmpty()) return "";
+  if(str.charAt(0) == 'x')  return endX(str.substring(1)) + 'x';
+  return str.charAt(0) + endX(str.substring(1));
+  
+}
