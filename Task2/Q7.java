ee989cbf766ebb322147ac2296c2430d26d134e9
V Dinesh Kumar <95033085+dineshmooley@users.noreply.github.com>
2024-05-15 21:31:08 +0530

    Update Q7.java

--- Task2/Q7.java
+++ Task2/Q7.java
@@ -3,6 +3,9 @@
 //a++ and a++ values only change after the execution of the declared line.
 //So, 28 + 29 + 28 + 28 = 113 and a = 28 + 113 = 141
 
+//15/05 09:23pm
+//a++ increments after the execution of the statement.
+//28 + 30 + 29 + 29 = 116 and a = 28 + 116 = 144   
 
 class Q7    {
     public static void main(String[] args)  {
