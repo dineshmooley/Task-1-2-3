0499118dffb5e227576e4b1dc8f35acd489884a8
V Dinesh Kumar <95033085+dineshmooley@users.noreply.github.com>
2024-05-15 18:52:21 +0530

    Create Q4.java

---
+++ Task2/Q4.java
@@ -0,0 +1,13 @@
+// 4) Write a program to print the number 7.50 without using String(print 7.50 not 7.5).
+
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args)  {
+        
+        double ans = 7.5;
+        System.out.printf("%.3f", ans);
+        
+    }
+}
