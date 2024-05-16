3fd63713ff99065615e2c79cf4add7d59c273962
V Dinesh Kumar <95033085+dineshmooley@users.noreply.github.com>
2024-05-16 11:35:45 +0530

    Create Q8.java

---
+++ Task2/Q8.java
@@ -0,0 +1,12 @@
+//8) Program to Find ASCII Value of a Character
+
+import java.util.Scanner;
+
+class Q8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        char ans = sc.next().charAt(0);
+        System.out.println((int) ans);
+    }
+}
