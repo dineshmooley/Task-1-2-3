32552e5ce6ba82d25cabbcbefbe4452459df2af8
V Dinesh Kumar <95033085+dineshmooley@users.noreply.github.com>
2024-05-17 19:05:06 +0530

    Create Q22.java

---
+++ Task2/Q22.java
@@ -0,0 +1,19 @@
+//22) Write a programs to print following patterns
+
+public class Q22 {
+    public static void main(String[] args) {
+        int n = 9;
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++)   {
+                System.out.print(" ");
+            }
+            
+            for(int j=1; j<=n-i; j++) {
+                System.out.print("* ");
+            }
+            
+            System.out.println();
+        }
+    }
+}
