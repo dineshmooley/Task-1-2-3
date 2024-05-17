45dad15b12a22fa08e5bd125ff21e6decc05c582
V Dinesh Kumar <95033085+dineshmooley@users.noreply.github.com>
2024-05-17 20:02:43 +0530

    Update Q25.java

--- Task2/Q25.java
+++ Task2/Q25.java
@@ -1,16 +1,25 @@
 //25) Write a program to print the following output for the given input. You can assume the string is of odd length
 
-class Q25    {
-    public static void main(String[] args)  {
+public class Q25 {
+    public static void main(String[] args) {
         
-        int n = 5;
+        String input = "12345";
+        int n = input.length();
+        int mid = n / 2;
         
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=n; j++)   {
-                if(j == i || j == n-i+1) System.out.print(j);
+        for (int i = 0; i <= mid; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == i || j == n - 1 - i) System.out.print(input.charAt(j));
                 else System.out.print(" ");
-            }   
-            
+            }
+            System.out.println();
+        }
+        
+        for (int i = mid - 1; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+                if (j == i || j == n - 1 - i) System.out.print(input.charAt(j));
+                else System.out.print(" ");
+            }
             System.out.println();
         }
     }
