cbfd28755f50bae3a373f35fa80d9a125750f9d9
V Dinesh Kumar <95033085+dineshmooley@users.noreply.github.com>
2024-05-17 19:14:25 +0530

    Update Q22.java

--- Task2/Q22.java
+++ Task2/Q22.java
@@ -5,11 +5,11 @@ public class Q22 {
         int n = 9;
         
         for(int i=1; i<=n; i++) {
-            for(int j=1; j<=i; j++)   {
+            for(int j=1; j<=i; j++) {
                 System.out.print(" ");
             }
             
-            for(int j=1; j<=n-i; j++) {
+            for(int j=1; j<=n-i+1; j++) {
                 System.out.print("* ");
             }
             
