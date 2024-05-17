82a731f54f5d15cb3f73422d7d63b0df1209fe8d
V Dinesh Kumar <95033085+dineshmooley@users.noreply.github.com>
2024-05-17 19:01:25 +0530

    Create Q21.java

---
+++ Task2/Q21.java
@@ -0,0 +1,23 @@
+//21) Write a programs to print following patterns
+
+public class Q21 {
+    public static void main(String[] args) {
+        int n = 9;
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=2*(n-i); j++)   {
+                System.out.print(" ");
+            }
+            
+            for(int j=1; j<=i; j++) {
+                System.out.print(j-i + 9 + " ");
+            }
+            
+            for(int j=i; j>=2; j--) {
+                System.out.print((j-1-i+9) + " ");
+            }
+            
+            System.out.println();
+        }
+    }
+}
