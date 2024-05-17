31487cbb1a7c137c8a8d4523ebf17b262adca133
V Dinesh Kumar <95033085+dineshmooley@users.noreply.github.com>
2024-05-17 11:38:31 +0530

    Update Q16.java

--- Task2/Q16.java
+++ Task2/Q16.java
@@ -1,8 +1,6 @@
 //16) Given two sorted arrays, merge them such that the elements are not repeated
 
-import java.util.Scanner;
-
-class HelloWorld {
+class Q16 {
     public static void main(String[] args) {
         int[] nums1 = {2, 4, 5, 6, 7, 9, 10, 13};
         int[] nums2 = {2, 3, 4, 5, 6, 7, 8, 9, 11, 15};
