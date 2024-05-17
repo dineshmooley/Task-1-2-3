83ce104405ec8fe87df4a76f526a50c9eaa8e769
V Dinesh Kumar <95033085+dineshmooley@users.noreply.github.com>
2024-05-17 19:56:16 +0530

    Update Q24.java

--- Task2/Q24.java
+++ Task2/Q24.java
@@ -1,21 +1,27 @@
 //24) Write a program to make such a pattern like a pyramid with numbers increased by 1.
 
-class Q24    {
-    public static void main(String[] args)  {
+public class Q24 {
+    public static void main(String[] args) {
+        int n = 10, rows = 0, elementsInRow = 1, temp = n;
+        while (temp > 0) {
+            rows++;
+            temp -= elementsInRow;
+            elementsInRow++;
+        }
         
-        int n = 4, count = 1;
-        
-        
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=n-i; j++)   {
+        int currentNumber = 1;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 0; j < rows - i; j++) {
                 System.out.print(" ");
-            }   
+            }
             
-            for(int j=1; j<=i; j++) {
-                System.out.print(count++ + " ");
+            for (int j = 1; j <= i && currentNumber <= n; j++) {
+                System.out.print(currentNumber + " ");
+                currentNumber++;
             }
             
             System.out.println();
         }
+
     }
 }
